37f49aed2d0f5f94bdca27f6d3a861f00303cb9f
YunchenBai <yunchen.bai@thoughtworks.com>
2020-06-14 23:03:36 +0800

    step 7

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksDaoTest.kt
@@ -0,0 +1,83 @@
+package com.example.android.architecture.blueprints.todoapp.data.source.local
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class TasksDaoTest {
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: ToDoDatabase
+
+    @Before
+    fun initDb() {
+        // Using an in-memory database so that the information stored here disappears when the
+        // process is killed.
+        database = Room.inMemoryDatabaseBuilder(
+                getApplicationContext(),
+                ToDoDatabase::class.java
+        ).build()
+    }
+
+    @After
+    fun closeDb() = database.close()
+
+    @Test
+    fun insertTaskAndGetById() = runBlockingTest {
+        // Given
+        val task = Task("title", "description")
+        database.taskDao().insertTask(task)
+
+        // when
+        val loaded = database.taskDao().getTaskById(task.id)
+
+        // Then
+        assertThat(loaded as Task, notNullValue())
+        assertThat(loaded.id, `is`(task.id))
+        assertThat(loaded.title, `is`(task.title))
+        assertThat(loaded.description, `is`(task.description))
+        assertThat(loaded.isCompleted, `is`(task.isCompleted))
+    }
+
+    @Test
+    fun updateTaskAndGetById() = runBlockingTest {
+        // 1. Insert a task into the DAO.
+        val task = Task("title", "description")
+        database.taskDao().insertTask(task)
+
+        // 2. Update the task by creating a new task with the same ID but different attributes.
+        val newTitle = "updated title"
+        val newDescription = "updated description"
+        task.title = newTitle
+        task.description = newDescription
+
+        database.taskDao().updateTask(task)
+
+        // 3. Check that when you get the task by its ID, it has the updated values.
+        val loaded = database.taskDao().getTaskById(task.id)
+
+        assertThat(loaded as Task, notNullValue())
+        assertThat(loaded.title, `is`(newTitle))
+        assertThat(loaded.description, `is`(newDescription))
+    }
+
+}
---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksLocalDataSourceTest.kt
@@ -0,0 +1,91 @@
+package com.example.android.architecture.blueprints.todoapp.data.source.local
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.succeeded
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matchers.`is`
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+class TasksLocalDataSourceTest {
+
+    private lateinit var localDataSource: TasksLocalDataSource
+    private lateinit var database: ToDoDatabase
+
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setup() {
+        // Using an in-memory database for testing, because it doesn't survive killing the process.
+        database = Room.inMemoryDatabaseBuilder(
+                ApplicationProvider.getApplicationContext(),
+                ToDoDatabase::class.java
+        )
+                .allowMainThreadQueries()
+                .build()
+
+        localDataSource =
+                TasksLocalDataSource(
+                        database.taskDao(),
+                        Dispatchers.Main
+                )
+    }
+
+    @After
+    fun cleanUp() {
+        database.close()
+    }
+
+    @Test
+    fun saveTask_retrievesTask() = runBlocking {
+        // GIVEN
+        val newTask = Task("title", "description", false)
+        localDataSource.saveTask(newTask)
+
+        // WHEN
+        val result = localDataSource.getTask(newTask.id)
+
+        // THEN
+        assertThat(result.succeeded, `is`(true))
+        result as Result.Success
+        assertThat(result.data.title, `is`("title"))
+        assertThat(result.data.description, `is`("description"))
+        assertThat(result.data.isCompleted, `is`(false))
+    }
+
+    @Test
+    fun completeTask_retrievedTaskIsComplete()= runBlocking {
+        // 1. Save a new active task in the local data source.
+        val task = Task("title", "description", false)
+        localDataSource.saveTask(task)
+
+        // 2. Mark it as complete.
+        localDataSource.completeTask(task)
+
+        // 3. Check that the task can be retrieved from the local data source and is complete.
+        val result = localDataSource.getTask(task.id)
+
+        result as Result.Success
+
+        assertThat(result.data.isCompleted, `is`(true))
+    }
+
+}
